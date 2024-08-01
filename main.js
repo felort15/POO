86fc50093864de158d59b4caaa4aaa2c341e2f6c
Felipe Ortiz <felort15@gmail.com>
2024-07-31 20:10:25 -0600

    Primer commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const natalia ={
+    name: "natalia",
+    age : 20,
+    cursosAprobados: [
+        "Curso definitivo HTML y CSS", "Curso practico HTML y CSS",
+    ],
+};
+//Hacer que natalia apruebe otro curso
+natalia.cursosAprobados .push("Curso de Responsive Design")
+
+
+
