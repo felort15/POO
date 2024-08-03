f7b9485901f96a12a14fbbcfcf36c1aee2bc90bb
Felipe Ortiz <felort15@gmail.com>
2024-08-02 20:17:57 -0600

    Objetos literales y prototipos

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
-const natalia={name:"Natalia",
-    age:20,
-    cursosAprobados:["Curso Definitivo de HTML y CSS","Curso Práctico de HTML y CSS",],
-    aprobarCurso(nuevoCursito){
-     this.cursosAprobados.push(nuevoCursito);
+class Student {
+    constructor(name, age, course, specialization) {
+      this.name = name;
+      this.age = age;
+      this.course = course;
+      this.specialization = specialization;
+      this.cursosAprobados = []; // initialize the array
     }
-   
-}
-natalia.cursosAprobados .push("Curso de Responsive Design");
-
-//Hacer que natalia apruebe otro curso
-
-
-function Student(name,age,cursosAprobados){
-    this.name= name;
-    this.age; this.cursosAprobados;
-}
-
-
-
+  
+    aprobarCurso(nuevoCursito) {
+      this.cursosAprobados.push(nuevoCursito);
+    }
+  }
+  
+  const natalia = new Student(
+    "Natalia",
+    20,
+    "Curso de diseño de interfaces de usuario",
+    "Diseño de interfaces de usuario"
+  );
+  
+  natalia.cursosAprobados.push("Curso de Responsive Design");
+  natalia.aprobarCurso("Curso de diseño de interfaces de usuario con CSS");
+  
+  console.log(natalia.cursosAprobados); 
+  // ["Curso de Responsive Design", "Curso de diseño de interfaces de usuario con CSS"]
+  
+  const juanita = new Student(
+    "Juanita Alejandra",
+    15,
+    "Curso de introducción a la producción de video juegos",
+    "Creación de personajes"
+  );
+  
+  juanita.aprobarCurso("Curso de programación en JavaScript");
+  console.log(juanita.cursosAprobados); // ["Curso de programación en JavaScript"]
