84a7ba9ae23ae2e57d32175ab3ef52695f813cd9
Felipe Ortiz <felort15@gmail.com>
2024-08-06 17:05:10 -0600

    Cambio a mjs

---
+++ module.mjs
@@ -0,0 +1,13 @@
+import { PlatziClass } from "./main.mjs";
+
+const clase67 = new PlatziClass({
+  name: "JavaScript: orientado a objetos, basado en prototipos",
+  videoID: "a-valid-video-id", // replace with a valid video ID
+});
+
+clase67.reproducir();
+clase67.pausar();
+clase67.reproducir();
+clase67.pausar();
+clase67.reproducir();
+clase67.pausar();
