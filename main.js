ca389f37b5638e3e5cc2f79d5f8c2ba8ec872d04
Felipe Ortiz <felort15@gmail.com>
2024-08-06 14:24:07 -0600

    Abstraccion JS

--- main.js
+++ main.js
@@ -1,85 +1,53 @@
+class Course {
+  constructor({ name }) {
+    this.name = name;
+    this.classes = [];
+  }
+}
+
+class LearningPath {
+  constructor({ name }) {
+    this.name = name;
+    this.courses = [];
+  }
+}
+
 class Student {
-  constructor({
-    name,
-    email,
-    username,
-    twitter = undefined,
-    instagram = undefined,
-    facebook = undefined,
-    approvedCourses = [],
-    learningPaths = [],
-  }) {
+  constructor({ name, email, username, twitter, instagram, facebook }) {
     this.name = name;
     this.email = email;
     this.username = username;
     this.socialMedia = { twitter, instagram, facebook };
-    this.approvedCourses = approvedCourses;
-    this.learningPaths = learningPaths;
-  }
-
-  aprobarCurso(nuevoCursito) {
-    this.approvedCourses.push(nuevoCursito);
+    this.approvedCourses = [];
+    this.learningPaths = [];
   }
 }
-  
-  const natalia = new Student(
-    "Natalia",
-    20,
-    "Curso de diseño de interfaces de usuario",
-    "Diseño de interfaces de usuario"
-  );
-  
-  natalia.approvedCourses.push("Curso de Responsive Design");
-  natalia.aprobarCurso("Curso de diseño de interfaces de usuario con CSS");
-  
-  console.log(natalia.cursosAprobados); 
-  // ["Curso de Responsive Design", "Curso de diseño de interfaces de usuario con CSS"]
-  
-  const juanita = new Student(
-    "Juanita Alejandra",
-    15,
-    "Curso de introducción a la producción de video juegos",
-    "Creación de personajes"
-  );
-  
-  juanita.aprobarCurso("Curso de programación en JavaScript");
-  console.log(juanita.cursosAprobados); // ["Curso de programación en JavaScript"]
 
-  class Student2 {
-    constructor({
-      name,
-      cursosAprobados = [],
-      age,
-      email,
-    }) {
-      this.name = name;
-      this.email = email;
-      this.age = age;
-      this.cursosAprobados = cursosAprobados;
-    }
-  
-    aprobarCurso(nuevoCursito) {
-      this.cursosAprobados.push(nuevoCursito);
-    }
-  }
-  
-  const miguelito = new Student2({
-    name: "Miguel",
-    age: 28,
-    email: "miguelito@platzi.com",
-  });
-  
-  console.log(miguelito);
-  // Output:
-  // Student2 {
-  //   name: 'Miguel',
-  //   email: 'miguelito@platzi.com',
-  //   age: 28,
-  //   cursosAprobados: []
-  // }
-  
-  miguelito.aprobarCurso("Curso de JavaScript");
-  console.log(miguelito.cursosAprobados);
-  // Output: ["Curso de JavaScript"]
+const cursoProgBasica = new Course({ name: "Curso Gratis de Programación Básica" });
+const cursoDefinitivoHTML = new Course({ name: "Curso Definitivo de HTML y CSS" });
+const cursoPracticoHTML = new Course({ name: "Curso Practico de HTML y CSS" });
 
- 
+const escuelaWeb = new LearningPath({ name: "Escuela de Desarrollo Web" });
+escuelaWeb.courses = [cursoProgBasica, cursoDefinitivoHTML, cursoPracticoHTML];
+
+const escuelaData = new LearningPath({ name: "Escuela de Data Science" });
+escuelaData.courses = [cursoProgBasica, "Curso DataBusiness", "Curso Dataviz"];
+
+const escuelaVgs = new LearningPath({ name: "Escuela de Vidweojuegos" });
+escuelaVgs.courses = [cursoProgBasica, "Curso de Unity", "Curso de Unreal"];
+
+const juan2 = new Student({
+  name: "JuanDC",
+  username: "juandc",
+  email: "juanito@juanito.com",
+  twitter: "fjuandc",
+});
+juan2.learningPaths = [escuelaWeb, escuelaVgs];
+
+const miguelito2 = new Student({
+  name: "Miguelito",
+  username: "migelitofeliz",
+  email: "miguelito@juanito.com",
+  instagram: "migelito_feliz",
+});
+miguelito2.learningPaths = [escuelaWeb, escuelaData];
